8e7e20a643954e49b0d1b483fa12f74e55c49e5b
dbsrl1026 <yungi1026@naver.com>
2024-03-20 23:57:47 +0900

    Add 9527

---
+++ 9527.py
@@ -0,0 +1,19 @@
+import sys
+import math
+sys.setrecursionlimit(10**8)
+a, b = map(int, input().split())
+
+
+def dnc(x):
+    if x <= 0:
+        return 0
+    jyo = int(math.log2(x))
+    floor_2pow = 2 ** jyo  # <= x 인 2의 ?승
+    if floor_2pow == x:
+        return jyo * x // 2 + 1
+
+    diff = x - floor_2pow
+    return dnc(floor_2pow) + diff + dnc(diff)
+
+
+print(dnc(b) - dnc(a-1))
