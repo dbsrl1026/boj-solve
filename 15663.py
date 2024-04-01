492715abcea672dd0b83e5e375d4be94a28602a2
dbsrl1026 <yungi1026@naver.com>
2024-04-01 22:47:03 +0900

    Add 15663

--- 15663.py
+++ 15663.py
@@ -1,22 +1,20 @@
-import copy
-import sys
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+visited = [False] * n
+temp = []
 
-n,m = map(int,sys.stdin.readline().split())
-num = sorted(list(map(int, sys.stdin.readline().split())))
-visited = []
-
-def dfs(cnt,arr,num):
-    if cnt <= 0:
-        if arr not in visited:
-            temp = copy.deepcopy(arr)
-            visited.append(temp)
-            print(*arr)
+def dfs():
+    if len(temp) == m:
+        print(*temp)
         return
+    remember_me = 0
     for i in range(n):
-        arr.append(num[i])
-        dfs(cnt - 1, arr)
-        arr.pop()
+        if not visited[i] and remember_me != nums[i]:
+            visited[i] = True
+            temp.append(nums[i])
+            remember_me = nums[i]
+            dfs()
+            visited[i] = False
+            temp.pop()
 
-
-for i in range(n):
-    dfs(m-1,[num[i]],num)
+dfs()
