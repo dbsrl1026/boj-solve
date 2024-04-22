467c14b90bf837136bd793d59958ca5033daf63a
dbsrl1026 <yungi1026@naver.com>
2024-04-22 10:53:50 +0900

    Add 1377

---
+++ 1377.py
@@ -0,0 +1,12 @@
+ n = int(input())
+arr = []
+for i in range(n):
+    arr.append((int(input()), i))
+
+sortedArr = sorted(arr)
+answer = []
+
+for i in range(n):
+    answer.append(sortedArr[i][1] - arr[i][1])
+
+print(max(answer) + 1)
